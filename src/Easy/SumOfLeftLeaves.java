e0350b08444d90eea10f27f4c05b8e1b9e8a5edd
Tanmay Shukla <tanmay.shukla629@gmail.com>
2024-04-14 12:42:59 +0530

    added the dfs approach for the sum of left leaves in BT

--- src/Easy/SumOfLeftLeaves.java
+++ src/Easy/SumOfLeftLeaves.java
@@ -15,9 +15,13 @@ public class SumOfLeftLeaves {
             this.right = right;
         }
     }
+
+    /**Function to check if the current node is a leaf node or not*/
     private boolean isLeaf(TreeNode root) {
         return root.left == null && root.right == null;
     }
+
+    /**Function to find sum of left leaf nodes via BFS traversal*/
     public int sumOfLeftLeaves(TreeNode root) {
         if (root == null) return 0;
         Queue<TreeNode> cache = new LinkedList<>();
@@ -25,15 +29,18 @@ public class SumOfLeftLeaves {
         int count = 0;
         while (!cache.isEmpty()) {
             TreeNode current = cache.poll();
+            // checking if left branch is not null
             if (current.left != null) {
+                // if left branch is a leaf node adding its value
                 if (isLeaf(current.left)) count += current.left.val;
-                cache.add(current.left);
             }
-            if (current.right != null) cache.add(current.right);
+            // adding non leaf and non-null right branches to the cache
+            if (current.right != null && !isLeaf(current.right)) cache.add(current.right);
         }
         return count;
     }
 
+    /**Using Pass by reference to get the sum of left leaves via DFS*/
     private void helper(TreeNode root, int[] answer) {
         if (root == null) return;
         if (root.left != null && isLeaf(root.left)) answer[0] += root.left.val;
@@ -41,6 +48,7 @@ public class SumOfLeftLeaves {
         helper(root.right, answer);
     }
 
+    /**Using basic recursion to get sum of left leaves via DFS*/
     private int helper2(TreeNode root) {
         if (root == null) return 0;
         int answer = 0;
@@ -48,6 +56,7 @@ public class SumOfLeftLeaves {
         return answer + helper2(root.left) + helper2(root.right);
     }
 
+    /**finding sum of left leaves via pass by reference or recursion or bfs*/
     public int SumOfLeaves2(TreeNode root) {
         if (root == null) return 0;
         int[] answer = {0};
