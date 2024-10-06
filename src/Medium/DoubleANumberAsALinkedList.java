39dd5034441cc97d82d64ceeced921c668121e1e
Tanmay Shukla <tanmay.shukla629@gmail.com>
2024-10-07 01:38:44 +0530

    last push not successful

--- src/Medium/DoubleANumberAsALinkedList.java
+++ src/Medium/DoubleANumberAsALinkedList.java
@@ -8,27 +8,42 @@ class ListNode {
         this.val = val;
         this.next = next;
     }
+
+    public ListNode(int val) {
+    }
 }
 
 public class DoubleANumberAsALinkedList {
-    public ListNode doubleIt(ListNode head) {
-        ListNode iterator = head;
-        ListNode next = head.next;
-        iterator.val = iterator.val * 2;
-        while (iterator.next != null) {
-            int newVal = next.val * 2;
-            int carry = newVal / 10;
-            newVal %= 10;
-            iterator.val += carry;
-            next.val = newVal;
-            iterator = iterator.next;
-            next = next.next;
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null) return head;
+        // creating a dummy node that points to the node to be returned
+        ListNode dummy = new ListNode(-1);
+        // initially assuming that head is unique value and setting it to the next one
+        dummy.next = head;
+        // creating another prev pointer that points to the last unique val node
+        ListNode prev = dummy;
+        // maintaining a key value for checking duplicates
+        int key = head.val;
+        // starting from the second node
+        head = head.next;
+        while (head != null) {
+            // if condition is satisfied node is not unique so we remove it
+            if (head.val == key) {
+                prev.next = null;
+                head = head.next;
+            } else {
+                // in this case either the val is not unique or we found another unique val
+                key = head.val;
+                // if there exists no next value then that we found no unique val in between prev and
+                // this unique value. If we have a next then that was not removed in the if condition
+                // and is unique so we set it as last unique value
+                if (prev.next != null) prev = prev.next;
+                // setting the next unique val to be next (will be removed if not unique)
+                prev.next = head;
+                head = head.next;
+            }
         }
-        if (head.val >= 10) {
-            ListNode temp = new ListNode(1, head);
-            head.val = head.val % 10;
-            return temp;
-        }
-        return head;
-    }
+        // return the values that are unique
+        return dummy.next;
+    }gi
 }
--- src/Medium/RemoveDuplicateNodes.java
+++ src/Medium/RemoveDuplicateNodes.java
@@ -1,4 +1,36 @@
 package Medium;
 
 public class RemoveDuplicateNodes {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null) return head;
+        // creating a dummy node that points to the node to be returned
+        ListNode dummy = new ListNode(-1);
+        // initially assuming that head is unique value and setting it to the next one
+        dummy.next = head;
+        // creating another prev pointer that points to the last unique val node
+        ListNode prev = dummy;
+        // maintaining a key value for checking duplicates
+        int key = head.val;
+        // starting from the second node
+        head = head.next;
+        while (head != null) {
+            // if condition is satisfied node is not unique so we remove it
+            if (head.val == key) {
+                prev.next = null;
+                head = head.next;
+            } else {
+                // in this case either the val is not unique or we found another unique val
+                key = head.val;
+                // if there exists no next value then that we found no unique val in between prev and
+                // this unique value. If we have a next then that was not removed in the if condition
+                // and is unique so we set it as last unique value
+                if (prev.next != null) prev = prev.next;
+                // setting the next unique val to be next (will be removed if not unique)
+                prev.next = head;
+                head = head.next;
+            }
+        }
+        // return the values that are unique
+        return dummy.next;
+    }
 }
